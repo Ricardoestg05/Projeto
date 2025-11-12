5ca43ecee0bc2bc3c6ec5f2f038d251ba82776d9
Simão das Neves Marques <2241603@my.ipleiria.pt>
2025-11-12 14:12:06 +0000

    Adicionei template nova de frontend

--- PROJETOweb/advanced/backend/views/site/login.php
+++ PROJETOweb/advanced/backend/views/site/login.php
@@ -3,22 +3,21 @@ use yii\helpers\Html;
 ?>
 <div class="d-flex align-items-center justify-content-center min-vh-100">
 
-    <div class="card login-card-custom">
+    <div class="card login-card-custom px-5 py-2">
         <div class="card-body login-card-body">
 
             <div class="text-center mb-4">
-                <img src="../../web/img/logoAthlos.png" alt="Logotipo" style="max-width: 120px;">
-                <h5 class="mt-2 text-muted">Athlos Gym</h5>
+                <img src="./img/logoAthlos.png" alt="Logotipo" style="max-width: 120px;">
+                <h5 class="mt-2 fw-bold">Athlos Gym</h5>
             </div>
 
-            <p class="login-box-msg">Faça login para começar a sua sessão</p>
+            <p class="login-box-msg">Faça login para começar a sua sessão:</p>
 
             <?php $form = \yii\bootstrap4\ActiveForm::begin(['id' => 'login-form']) ?>
 
             <?= $form->field($model,'username', [
                 // ... (restante do código do campo username) ...
                 'options' => ['class' => 'form-group has-feedback'],
-                'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>',
                 'template' => '{beginWrapper}{input}{error}{endWrapper}',
                 'wrapperOptions' => ['class' => 'input-group mb-3']
             ])
@@ -28,7 +27,6 @@ use yii\helpers\Html;
             <?= $form->field($model, 'password', [
                 // ... (restante do código do campo password) ...
                 'options' => ['class' => 'form-group has-feedback'],
-                'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>',
                 'template' => '{beginWrapper}{input}{error}{endWrapper}',
                 'wrapperOptions' => ['class' => 'input-group mb-3']
             ])
@@ -36,99 +34,16 @@ use yii\helpers\Html;
                 ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
 
             <div class="row">
-                <div class="col-8">
-                    <?= $form->field($model, 'rememberMe')->checkbox([
-                        'template' => '<div class="icheck-primary">{input}{label}</div>',
-                        'labelOptions' => [
-                            'class' => ''
-                        ],
-                        'uncheck' => null
-                    ]) ?>
-                </div>
-                <div class="col-4">
+
+                <div class="col">
                     <?= Html::submitButton('Sign In', ['class' => 'btn btn-primary w-100']) ?>
                 </div>
             </div>
 
             <?php \yii\bootstrap4\ActiveForm::end(); ?>
 
-            <div class="social-auth-links text-center mb-3">
-                <p>- OR -</p>
-                <a href="#" class="btn btn-block btn-primary btn-facebook">
-                    <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                </a>
-                <a href="#" class="btn btn-block btn-danger btn-google">
-                    <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-                </a>
-            </div>
-            <p class="mb-1 text-center">
-                <a href="forgot-password.html">I forgot my password</a>
-            </p>
-            <p class="mb-0 text-center">
-                <a href="register.html" class="text-center">Register a new membership</a>
-            </p>
+
         </div>
     </div>
 </div>
 
-<div class="card">
-    <div class="card-body login-card-body">
-        <p class="login-box-msg">Sign in to start your session</p>
-
-        <?php $form = \yii\bootstrap4\ActiveForm::begin(['id' => 'login-form']) ?>
-
-        <?= $form->field($model,'username', [
-            'options' => ['class' => 'form-group has-feedback'],
-            'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>',
-            'template' => '{beginWrapper}{input}{error}{endWrapper}',
-            'wrapperOptions' => ['class' => 'input-group mb-3']
-        ])
-            ->label(false)
-            ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
-
-        <?= $form->field($model, 'password', [
-            'options' => ['class' => 'form-group has-feedback'],
-            'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>',
-            'template' => '{beginWrapper}{input}{error}{endWrapper}',
-            'wrapperOptions' => ['class' => 'input-group mb-3']
-        ])
-            ->label(false)
-            ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
-
-        <div class="row">
-            <div class="col-8">
-                <?= $form->field($model, 'rememberMe')->checkbox([
-                    'template' => '<div class="icheck-primary">{input}{label}</div>',
-                    'labelOptions' => [
-                        'class' => ''
-                    ],
-                    'uncheck' => null
-                ]) ?>
-            </div>
-            <div class="col-4">
-                <?= Html::submitButton('Sign In', ['class' => 'btn btn-primary btn-block']) ?>
-            </div>
-        </div>
-
-        <?php \yii\bootstrap4\ActiveForm::end(); ?>
-
-        <div class="social-auth-links text-center mb-3">
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-primary">
-                <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-            </a>
-            <a href="#" class="btn btn-block btn-danger">
-                <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-            </a>
-        </div>
-        <!-- /.social-auth-links -->
-
-        <p class="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
-        </p>
-        <p class="mb-0">
-            <a href="register.html" class="text-center">Register a new membership</a>
-        </p>
-    </div>
-    <!-- /.login-card-body -->
-</div>
